164eeb414a6e28257d55becb38ecb5d5eb0415ae
GimpHJ <devhjlim@gmail.com>
2018-02-04 23:04:22 -0800

    Change quiz

--- Lesson 21/project-pixel-art-maker-starter-master/designs.js
+++ Lesson 21/project-pixel-art-maker-starter-master/designs.js
@@ -2,70 +2,46 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
-$(function(){
-
-	const sizePickerForm = $('#sizePicker');
-	const inputHeight = $('#inputHeight');
-	const inputWeight = $('#inputWeight');
-	const pixelCanvas = $('#pixelCanvas');
-	const colorPicker = $('#colorPicker');
-
-
-	sizePickerForm.submit(function(event){
+function init(){
+	$('#sizePicker').submit(function(event){
 		event.preventDefault();
-		makeGrid(inputHeight.val(), inputWeight.val());
-	});
+		makeGrid();
+	});	
+}
 
-
-	pixelCanvas.on('click', 'td', function(event){
-		fillColor($(this), colorPicker.val());
-	});
-
-	pixelCanvas.on('mousemove mouseover', 'td', function(event){
-		//When pressing left click mouse
-		if(event.which === 1 ){
-				fillColor($(this), colorPicker.val());
-		}
-	});
-
-	/**
-	* @description Fill a color
-	* @constructor
-	* @param {object} item - The object that should be colored.
-	* @param {string} color - The value of color
-	*/
-	function fillColor(item, color){
-		$(item).css('background-color', color);
+/**
+* @description Make the grid table
+*/
+function makeGrid() {
+	const table = document.getElementById('pixelCanvas');
+	
+	//Delete previous rows.
+	while(table.rows.length > 0){
+		table.deleteRow(0);
 	}
 
-	/**
-	* @description Make the grid table
-	* @constructor
-	* @param {number} height - The number of table height
-	* @param {number} weight - The number of table weight
-	*/
-	function makeGrid(height, weight) {
-
-		let tr = '';
-
-		let h = 0;
-
-		while( h < height){
-
-			tr += '<tr>';		
-
-				for(var w = 0; w < weight; w++){
-					tr += '<td></td>';
-				}
-
-			tr += '</tr>';
-
-			h++;
+	//Make grid
+	for(let i = 0; i < $('#inputHeight').val(); i++){
+			//isnert row
+			const row = table.insertRow(i);
+		for(let j=0; j< $('#inputWeight').val(); j++){
+			//insert cell
+			const cell = row.insertCell(j);
+			//add event listener each cell.
+			cell.addEventListener('click', fillColor);
 		}
-
-		pixelCanvas.html(tr);
 	}
+}
 
-});
+/**
+* @description Fill a color
+* @param {object} item - The object that should be colored.
+*/
+function fillColor(){
+	$(this).css('background-color', $('#colorPicker').val());
+}
+
+$(document).ready(init);
+
+	
 
